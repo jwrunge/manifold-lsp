c83e199dd3bc2ac0604abe7fa3a1d56599850fc9
jwr <jwrunge@gmail.com>
2025-10-06 22:23:12 -0500

    Working client/server scaffold

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,14 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use tokio::net::TcpListener;
 use tower_lsp::{
     async_trait,
+    jsonrpc::Error,
     lsp_types::{
-        notification::Notification, ExecuteCommandOptions, InitializeParams, InitializeResult,
-        ServerCapabilities,
+        notification::Notification, ExecuteCommandOptions, ExecuteCommandParams, InitializeParams,
+        InitializeResult, MessageType, ServerCapabilities,
     },
-    Client, LanguageServer,
+    Client, LanguageServer, LspService, Server,
 };
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -19,7 +22,6 @@ enum CustomNotification {}
 
 impl Notification for CustomNotification {
     type Params = NotificationParams;
-
     const METHOD: &'static str = "custom/notification";
 }
 
@@ -30,15 +32,59 @@ struct Backend {
 
 #[async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        OK(InitializeResult {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult, Error> {
+        Ok(InitializeResult {
             server_info: None,
             capabilities: ServerCapabilities {
                 execute_command_provider: Some(ExecuteCommandOptions {
-                    commands: vec![String::from("custom/notification")],
-                    work_done_progress_options: None,
+                    commands: vec![String::from("custom.notification")],
+                    work_done_progress_options: Default::default(),
                 }),
+                ..ServerCapabilities::default()
             },
+            ..Default::default()
         })
     }
+
+    async fn shutdown(&self) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn execute_command(&self, params: ExecuteCommandParams) -> Result<Option<Value>, Error> {
+        if params.command == "custom.notification" {
+            self.client
+                .send_notification::<CustomNotification>(NotificationParams {
+                    title: String::from("Hello!"),
+                    message: String::from("This is a custom notification."),
+                    description: String::from("Sent from the Rust LSP server."),
+                })
+                .await;
+
+            self.client
+                .log_message(
+                    MessageType::INFO,
+                    format!("Command executited successfully with params: {:?}", params),
+                )
+                .await;
+
+            Ok(None)
+        } else {
+            Err(Error::invalid_request())
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let listener: TcpListener = tokio::net::TcpListener::bind("127.0.0.1:8081")
+        .await
+        .unwrap();
+
+    let (stream, _) = listener.accept().await.unwrap();
+    let (read, write) = tokio::io::split(stream);
+    let (service, socket) = LspService::new(|client| Backend { client });
+
+    Server::new(read, write, socket).serve(service).await;
 }
